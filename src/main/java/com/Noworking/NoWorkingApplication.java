10e5a1721b3aba34641176e5ec79401d90845ad9
karan.sahu <karan.sahu@metop.sapphireims.dev>
2024-07-30 22:52:45 +0530

    Initial commit

---
+++ src/main/java/com/Noworking/NoWorkingApplication.java
@@ -0,0 +1,13 @@
+package com.Noworking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoWorkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoWorkingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Noworking/NoWorkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Noworking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoWorkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
